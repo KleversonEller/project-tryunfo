0156a39cb985c3772be2358e6c8b772737972d52
Kleverson Eller <kleverson.a16@gmail.com>
2022-02-24 04:50:17 -0300

    Requisito 11

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -7,52 +7,71 @@ class CardList extends React.Component {
     super();
     this.state = {
       filterName: '',
+      filterRaridade: 'todas',
     };
 
     this.filter = this.filter.bind(this);
   }
 
-  filter({ target: { value } }) {
-    this.setState({ filterName: value });
+  filter({ target: { value, name } }) {
+    this.setState({ [name]: value });
   }
 
   render() {
     const { list, delet } = this.props;
-    const { filterName } = this.state;
+    const { filterName, filterRaridade } = this.state;
     return (
       <div>
         <label htmlFor="filter-name">
-          Procurar carta:
+          Filtrar nome:
           <input
+            name="filterName"
             onChange={ this.filter }
             id="filter-name"
             data-testid="name-filter"
             type="text"
           />
         </label>
-        {list.filter((objeto) => objeto.name.includes(filterName)).map((objeto) => (
-          (
-            <div key={ objeto.name }>
-              <Card
-                cardName={ objeto.name }
-                cardDescription={ objeto.description }
-                cardAttr1={ objeto.attr1 }
-                cardAttr2={ objeto.attr2 }
-                cardAttr3={ objeto.attr3 }
-                cardImage={ objeto.image }
-                cardRare={ objeto.rare }
-                cardTrunfo={ objeto.trunfo }
-              />
-              <button
-                onClick={ delet }
-                type="button"
-                data-testid="delete-button"
-                name={ objeto.name }
-              >
-                Excluir
-              </button>
-            </div>)
-        ))}
+        <label htmlFor="filter-raridade">
+          Filtrar raridade:
+          <select
+            name="filterRaridade"
+            onChange={ this.filter }
+            id="filter-raridade"
+            data-testid="rare-filter"
+          >
+            <option value="todas">todas</option>
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        {list.filter((objeto) => objeto.name.includes(filterName))
+          .filter((objeto) => (filterRaridade === 'todas'
+            ? objeto
+            : objeto.rare === filterRaridade)).map((objeto) => (
+            (
+              <div key={ objeto.name }>
+                <Card
+                  cardName={ objeto.name }
+                  cardDescription={ objeto.description }
+                  cardAttr1={ objeto.attr1 }
+                  cardAttr2={ objeto.attr2 }
+                  cardAttr3={ objeto.attr3 }
+                  cardImage={ objeto.image }
+                  cardRare={ objeto.rare }
+                  cardTrunfo={ objeto.trunfo }
+                />
+                <button
+                  onClick={ delet }
+                  type="button"
+                  data-testid="delete-button"
+                  name={ objeto.name }
+                >
+                  Excluir
+                </button>
+              </div>)
+          ))}
       </div>
     );
   }
