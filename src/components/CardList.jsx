a195d1804947dc469aa83c56a6fb672cbae75a89
Kleverson Eller <kleverson.a16@gmail.com>
2022-02-24 04:19:05 -0300

    Requisito 10

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,31 +3,55 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 class CardList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filterName: '',
+    };
+
+    this.filter = this.filter.bind(this);
+  }
+
+  filter({ target: { value } }) {
+    this.setState({ filterName: value });
+  }
+
   render() {
     const { list, delet } = this.props;
+    const { filterName } = this.state;
     return (
       <div>
-        {list.map((objeto) => (
-          <div key={ objeto.name }>
-            <Card
-              cardName={ objeto.name }
-              cardDescription={ objeto.description }
-              cardAttr1={ objeto.attr1 }
-              cardAttr2={ objeto.attr2 }
-              cardAttr3={ objeto.attr3 }
-              cardImage={ objeto.image }
-              cardRare={ objeto.rare }
-              cardTrunfo={ objeto.trunfo }
-            />
-            <button
-              onClick={ delet }
-              type="button"
-              data-testid="delete-button"
-              name={ objeto.name }
-            >
-              Excluir
-            </button>
-          </div>
+        <label htmlFor="filter-name">
+          Procurar carta:
+          <input
+            onChange={ this.filter }
+            id="filter-name"
+            data-testid="name-filter"
+            type="text"
+          />
+        </label>
+        {list.filter((objeto) => objeto.name.includes(filterName)).map((objeto) => (
+          (
+            <div key={ objeto.name }>
+              <Card
+                cardName={ objeto.name }
+                cardDescription={ objeto.description }
+                cardAttr1={ objeto.attr1 }
+                cardAttr2={ objeto.attr2 }
+                cardAttr3={ objeto.attr3 }
+                cardImage={ objeto.image }
+                cardRare={ objeto.rare }
+                cardTrunfo={ objeto.trunfo }
+              />
+              <button
+                onClick={ delet }
+                type="button"
+                data-testid="delete-button"
+                name={ objeto.name }
+              >
+                Excluir
+              </button>
+            </div>)
         ))}
       </div>
     );
